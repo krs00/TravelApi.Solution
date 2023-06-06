397828f6d0b32bc69e397c646e7aec881f48558b
Kymani Stephens <kymanist00@gmail.com>
2023-06-05 22:09:14 -0700

    update file to use MySQL and db context

--- TravelApi/Program.cs
+++ TravelApi/Program.cs
@@ -1,23 +1,33 @@
+using TravelApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<TravelApiContext>(
+                  dbContextOptions => dbContextOptions
+                    .UseMySql(
+                      builder.Configuration["ConnectionStrings:DefaultConnection"], 
+                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                    )
+                  )
+                );
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.UseSwagger();
-  app.UseSwaggerUI(); 
-}
-else
+    app.UseSwagger();
+    app.UseSwaggerUI();
+} 
+else 
 {
-    app.UseHttpsRedirection(); 
+  app.UseHttpsRedirection();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
