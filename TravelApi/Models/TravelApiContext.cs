8b070d48088601d7adac09b5519d67296760512f
Kymani Stephens <kymanist00@gmail.com>
2023-06-05 23:43:33 -0700

    add seed data

--- TravelApi/Models/TravelApiContext.cs
+++ TravelApi/Models/TravelApiContext.cs
@@ -10,5 +10,28 @@ namespace TravelApi.Models
     public TravelApiContext(DbContextOptions<TravelApiContext> options) : base(options)
     {
     }
+
+    protected override void OnModelCreating(ModelBuilder builder) // Seeded Data for API with places from Earthbound
+    {
+      builder.Entity<Destination>()
+        .HasData(
+          new Destination { DestinationId = 1, Name = "Onett", Location = "Eagleland", Description = "Small town" },
+          new Destination { DestinationId = 2, Name = "Twoson", Location = "Eagleland", Description = "Big town" },
+          new Destination { DestinationId = 3, Name = "Magicant", Location = "Dreamland", Description = "Quirky" },
+          new Destination { DestinationId = 4, Name = "Dalaam", Location = "Chommo", Description = "Rural village" }
+        );
+
+      builder.Entity<Review>()
+        .HasData(
+          new Review { ReviewId = 1, DestinationId = 1, Comment = "Kind of boring", Rating = 8, Date = DateTime.Now },
+          new Review { ReviewId = 2, DestinationId = 2, Comment = "cool place", Rating = 5, Date = DateTime.Now },
+          new Review { ReviewId = 3, DestinationId = 3, Comment = "Weird", Rating = 6, Date = DateTime.Now },
+          new Review { ReviewId = 4, DestinationId = 4, Comment = "good vibes", Rating = 5, Date = DateTime.Now }
+        );
+    }
+
+
+
+
   }
 }
