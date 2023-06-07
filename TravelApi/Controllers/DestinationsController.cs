27a7da0f0b479dd5f3af0ee797920b281d1e8e6f
Kymani Stephens <kymanist00@gmail.com>
2023-06-07 01:04:45 -0700

    add delete functionality

--- TravelApi/Controllers/DestinationsController.cs
+++ TravelApi/Controllers/DestinationsController.cs
@@ -87,6 +87,22 @@ namespace TravelApi.Controllers
       return _db.Destinations.Any(e => e.DestinationId == id);
     }
 
+		    // DELETE: api/Destinations/3
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteDestination(int id)
+    {
+      Destination destination = await _db.Destinations.FindAsync(id);
+      if (destination == null)
+      {
+        return NotFound();
+      }
+
+      _db.Destinations.Remove(destination);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+
 
 
 
